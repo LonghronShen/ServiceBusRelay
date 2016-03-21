cfce2aa83db233cb9f82984d3e7966f91918eaf2
y-z-f <976691141@qq.com>
2016-03-21 13:27:53 +0800

    完成基于命令行的客户端开发

--- EchoClient/Program.cs
+++ EchoClient/Program.cs
@@ -10,16 +10,10 @@ namespace EchoClient
 		{
 			ServiceBusEnvironment.SystemConnectivity.Mode = ConnectivityMode.AutoDetect;
 
-			Uri serviceUri = ServiceBusEnvironment.CreateServiceUri("sb", "sinoapi", "EchoService");
-			TransportClientEndpointBehavior sasCredential = new TransportClientEndpointBehavior();
-			sasCredential.TokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider("RootManageSharedAccessKey", "key");
-
-			ChannelFactory<IEchoChannel> channelFactory = new ChannelFactory<IEchoChannel>("RelayEndpoint", new EndpointAddress(serviceUri));
-			channelFactory.Endpoint.EndpointBehaviors.Add(sasCredential);
-
+			ChannelFactory<IEchoChannel> channelFactory = new ChannelFactory<IEchoChannel>("RelayEndpoint");
 			IEchoChannel channel = channelFactory.CreateChannel();
 			channel.Open();
-
+			
 			Console.WriteLine("Enter text to echo (or [Enter] to exit):");
 			string input = Console.ReadLine();
 			while(input != string.Empty)
